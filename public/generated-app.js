7666bdc3a3a805e2c476b42c08820154847f0989
Code <code@sedc.mk>
2018-09-26 20:35:20 +0200

    final part lesson-1

--- public/generated-app.js
+++ public/generated-app.js
@@ -19130,7 +19130,15 @@ module.exports = require('./lib/React');
 },{"./lib/React":53}],159:[function(require,module,exports){
 'use strict';
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
@@ -19140,20 +19148,119 @@ var _reactDom = require('react-dom');
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var Hello = function Hello(name) {
+var Square = function Square(props) {
     return _react2['default'].createElement(
-        'div',
-        null,
-        'Hello ',
-        name,
-        ' how are you'
+        'button',
+        { className: 'square', onClick: function () {
+                return props.onClick();
+            } },
+        props.value
     );
 };
 
-var view = Hello("Borche");
+var Board = (function (_React$Component) {
+    _inherits(Board, _React$Component);
 
-var element = document.getElementById("app");
+    function Board(props) {
+        _classCallCheck(this, Board);
 
-_reactDom2['default'].render(view, element);
+        _get(Object.getPrototypeOf(Board.prototype), 'constructor', this).call(this, props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    }
+
+    _createClass(Board, [{
+        key: 'handleClick',
+        value: function handleClick(i) {
+            var squares = this.state.squares.slice();
+            squares[i] = this.state.xIsNext ? "X" : "O";
+            this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+        }
+    }, {
+        key: 'renderSquere',
+        value: function renderSquere(i) {
+            var _this = this;
+
+            return _react2['default'].createElement(Square, { value: this.state.squares[i], onClick: function () {
+                    return _this.handleClick(i);
+                } });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+
+            return _react2['default'].createElement(
+                'div',
+                null,
+                _react2['default'].createElement(
+                    'div',
+                    { className: 'status' },
+                    status
+                ),
+                _react2['default'].createElement(
+                    'div',
+                    { className: 'board-row' },
+                    this.renderSquere(0),
+                    ' ',
+                    this.renderSquere(1),
+                    ' ',
+                    this.renderSquere(2)
+                ),
+                _react2['default'].createElement(
+                    'div',
+                    { className: 'board-row' },
+                    this.renderSquere(3),
+                    ' ',
+                    this.renderSquere(4),
+                    ' ',
+                    this.renderSquere(5)
+                ),
+                _react2['default'].createElement(
+                    'div',
+                    { className: 'board-row' },
+                    this.renderSquere(6),
+                    ' ',
+                    this.renderSquere(7),
+                    ' ',
+                    this.renderSquere(8)
+                )
+            );
+        }
+    }]);
+
+    return Board;
+})(_react2['default'].Component);
+
+var Game = (function (_React$Component2) {
+    _inherits(Game, _React$Component2);
+
+    function Game() {
+        _classCallCheck(this, Game);
+
+        _get(Object.getPrototypeOf(Game.prototype), 'constructor', this).apply(this, arguments);
+    }
+
+    _createClass(Game, [{
+        key: 'render',
+        value: function render() {
+            return _react2['default'].createElement(
+                'div',
+                { className: 'game' },
+                _react2['default'].createElement(
+                    'div',
+                    { className: 'game-board' },
+                    _react2['default'].createElement(Board, null)
+                )
+            );
+        }
+    }]);
+
+    return Game;
+})(_react2['default'].Component);
+
+_reactDom2['default'].render(_react2['default'].createElement(Game, null), document.getElementById("app"));
 
 },{"react":158,"react-dom":29}]},{},[159]);
---
+++ src/example1.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+
+const Square = (props) => {
+    return (
+        <button className="square" onClick={() => props.onClick()}>{props.value}</button>
+    );
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+    }
+
+    renderSquere(i) {
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    }
+
+    render() {
+        const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquere(0)} {this.renderSquere(1)} {this.renderSquere(2)}
+                </div>
+
+                <div className="board-row">
+                    {this.renderSquere(3)} {this.renderSquere(4)} {this.renderSquere(5)}
+                </div>
+
+                <div className="board-row">
+                    {this.renderSquere(6)} {this.renderSquere(7)} {this.renderSquere(8)}
+                </div>
+            </div>
+        )
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+
+                <div className="game-board">
+                    <Board />
+                </div>
+
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Game />, document.getElementById("app"));
